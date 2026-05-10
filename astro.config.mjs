ddd70354f7a3a92668db3cab281cd5a3e35c334e
postite <david@postite.com>
2026-05-11 00:57:30 +0200

    less+ txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,16 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   // Remplace par ton domaine (ex: https://www.tonsite.com)
   // Ou par ton URL github : https://pseudo.github.io
-  site: 'https://www.dogwalkeuse.com', 
+  site: 'https://www.dogwalkeuse.com',
   // Si le site est à la racine du domaine, laisse '/'
-  base: '/', 
+  base: '/',
+  vite: {
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
+        },
+      },
+    },
+  },
 });
